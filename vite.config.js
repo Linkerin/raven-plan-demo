5150106e8fcf11ec4be1bffa06ec8f32c29abec6
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-05-13 18:13:35 +0300

    UPD: one more try with vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/raven-plan-demo/',
-//   plugins: [react()]
-// });
-
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/'
-  };
-
-  if (command !== 'serve') {
-    config.base = '/raven-plan-demo/';
-  }
-
-  return config;
+//vitejs.dev/config/
+export default defineConfig({
+  base: '/raven-plan-demo/',
+  plugins: [react()]
 });
